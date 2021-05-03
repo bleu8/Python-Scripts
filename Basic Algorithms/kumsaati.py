6802febeb5b730fb90aa7f815e5ab0510bebf96e
Fidelio <78764084+ethereal03@users.noreply.github.com>
2021-05-03 21:11:16 +0300

    Add files via upload

---
+++ Basic Algorithms/kumsaati.py
@@ -0,0 +1,12 @@
+#5den 1e dogru giden bir program
+
+import time
+
+i=5
+
+while i>=0:
+    print(i)
+    i=i-1
+    time.sleep(1)
+    
+ #baslangıc 5 degisim -1 sart: 0 
---
+++ Basic Algorithms/love.py
@@ -0,0 +1,10 @@
+import turtle
+
+turtle.bgcolor("black")
+turtle.pensize(2)
+
+
+def curve():
+    for i in range(200):
+        turtle.right(1)
+        turtle.forward(1)
